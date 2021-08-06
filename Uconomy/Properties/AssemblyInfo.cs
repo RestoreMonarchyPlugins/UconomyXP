c301c2b117f152d1f674b814dc6275f11394c8c3
RestoreMonarchy <35507241+RestoreMonarchy@users.noreply.github.com>
2021-08-06 18:37:48 +0200

    renamed to uconomyxp

--- Uconomy/Properties/AssemblyInfo.cs
+++ Uconomy/Properties/AssemblyInfo.cs
@@ -2,9 +2,9 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("UconomyToExperience")]
-[assembly: AssemblyCompany("unturned.ROCKS")]
-[assembly: AssemblyProduct("Rocket")]
+[assembly: AssemblyTitle("UconomyXP")]
+[assembly: AssemblyCompany("Restore Monarchy")]
 [assembly: ComVisible(false)]
 [assembly: Guid("34ca52ac-333c-45df-8a77-8c8f7c463faa")]
 [assembly: AssemblyVersion("1.1.0")]
+[assembly: AssemblyFileVersion("1.1.0")]
